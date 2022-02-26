57d089d2125e0071f7076cd9a1014212e61c92f7
Franklin Fargoj <franklin.f@ergode.com>
2022-02-26 14:00:12 +0530

    Initial stage of application created first controller

---
+++ application/controllers/ChatApplicationController.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ChatApplicationController extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('ChatApplication_model', 'table');
+
+	}
+
+
+	public function index()
+	{
+		$userData = $this->table->getData('*','user');
+		echo "<pre>";
+		print_r($userData);
+	}
+}
---
+++ application/models/ChatApplication_model.php
@@ -0,0 +1,28 @@
+<?php
+
+class ChatApplication_model extends CI_Model  
+{
+    public function __construct() 
+	{
+        parent::__construct();
+        $this->Db = $this->load->database('default',true);
+    }
+
+	//get User Data manually
+    public function getData($select, $from, $where = ''){
+
+    	$select = (!empty($select))?$select:'*';
+
+        if(!empty($where))
+            $sql = "SELECT $select FROM $from WHERE $where";
+        else
+            $sql = "SELECT $select FROM $from";
+        return $this->Db->query($sql)->result_array();
+
+    }
+    
+
+ }
+
+
+ ?>
